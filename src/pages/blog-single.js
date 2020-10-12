7aa5211ca710602047d483d19e89396c4b4afa19
error6251 <juhair98@gmail.com>
2020-10-12 16:07:17 +0600

    added: test netlifier cms

--- src/pages/blog-single.js
+++ src/pages/blog-single.js
@@ -15,7 +15,7 @@ export default function BlogPost({ data }) {
 }
 
 export const query = graphql`
-  query($slug: String!) {
+  query($slug: String) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
